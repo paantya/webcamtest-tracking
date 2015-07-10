b9459b67da963317db746b12a44d0402b4011282
Pavel Vasilev <explosere@ya.ru>
2015-07-10 14:03:03 +0300

    Project cleanup

--- TimingsDebug.h
+++ TimingsDebug.h
@@ -1,10 +1,11 @@
 #define DEBUG_HELI_TIMINGS
 
+// illegal things happen below
 #ifdef DEBUG_HELI_TIMINGS
 #include <QtCore\qelapsedtimer.h>
-#define TimerCreate() QElapsedTimer QET
-#define TimerUpdate() QET.start()
-#define TimerElapsed() qint64 te = QET.elapsed(); if(te>0) printf("Thread %s ran for %i ms\n", __FILE__, te)
+#define TimerCreate() QElapsedTimer __QET
+#define TimerUpdate() __QET.start()
+#define TimerElapsed() qint64 __te = __QET.elapsed(); if(__te>0) printf("Thread %s ran for %i ms\n", __FILE__, __te)
 #else
 #define TimerCreate()
 #define TimerUpdate()
